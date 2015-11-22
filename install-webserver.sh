cc8e2b6e1b62d353fbc0920a3b64faac3dda77b1
UzmaFarheen <unln@hawk.iit.edu>
2015-11-21 21:00:25 -0600

    new webserver script

--- install-webserver.sh
+++ install-webserver.sh
@@ -3,6 +3,16 @@
 sudo apt-get -y update
 sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql
 
+git clone https://github.com/UzmaFarheen/ITMO544-localinstance.git
+git clone https://github.com/UzmaFarheen/MP1-PHP.git
+
+sudo mv ./MP1-PHP/index.html /var/www/html
+sudo mv ./MP1-PHP/page2.html /var/www/html
+sudo mv ./MP1-PHP/*.css /var/www/html
+sudo mv ./MP1-PHP/*.js /var/www/html
+sudo mv ./MP1-PHP/*.php /var/www/html
+sudo mv ./ITMO544-localinstance/images/*.jpeg /var/www/html
+
 curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
 
 sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
